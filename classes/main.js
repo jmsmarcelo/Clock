a84a60c2f354552af0972e56dfa945c849f78cfe
jmsmarcelo <32857346+jmsmarcelo@users.noreply.github.com>
2023-05-18 19:59:24 -0400

    Optimized

--- classes/main.js
+++ classes/main.js
@@ -1,7 +1,20 @@
-var d = document, dColon = s[1], oTemp, tM = ['', '', '', '', ''];
+var d = document, s = ['none', 'block'], dColon = s[1], oTemp, tM = ['', '', '', '', ''],
+    maps = [
+        [s[1], s[1], s[1], s[0], s[1], s[1], s[1]],
+        [s[0], s[0], s[1], s[0], s[0], s[1], s[0]],
+        [s[1], s[0], s[1], s[1], s[1], s[0], s[1]],
+        [s[1], s[0], s[1], s[1], s[0], s[1], s[1]],
+        [s[0], s[1], s[1], s[1], s[0], s[1], s[0]],
+        [s[1], s[1], s[0], s[1], s[0], s[1], s[1]],
+        [s[1], s[1], s[0], s[1], s[1], s[1], s[1]],
+        [s[1], s[0], s[1], s[0], s[0], s[1], s[0]],
+        [s[1], s[1], s[1], s[1], s[1], s[1], s[1]],
+        [s[1], s[1], s[1], s[1], s[0], s[1], s[1]]
+    ];
+    
 setInterval(function() {
-    let t = new Date(), nTemp;
-    t.toString().replace(/(\d)(\d):(\d)(\d):(\d)(\d)/, function(m, h0, h1, m0, m1, s0, s1) {
+    let nTemp;
+    Date().toString().replace(/(\d)(\d):(\d)(\d):(\d)(\d)/, function(m, h0, h1, m0, m1, s0, s1) {
         if(tM[0] != h0) setNumber('h0-', h0);
         if(tM[1] != h1) setNumber('h1-', h1);
         if(tM[2] != m0) setNumber('m0-', m0);
