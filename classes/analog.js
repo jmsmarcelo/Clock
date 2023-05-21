3d9b61e2fbb8013e652d46782c7381d01ff72842
jmsmarcelo <32857346+jmsmarcelo@users.noreply.github.com>
2023-05-21 11:46:42 -0400

    Fix position of pointers

--- classes/analog.js
+++ classes/analog.js
@@ -1,9 +1,7 @@
-var d = document, h = '00', m = '00', s = '00';
-
 setInterval(function() {
     Date().toString().replace(/(\d{2}):(\d{2}):(\d{2})/, function(match, hh, mm, ss) {
         if(h != hh) setAnalog('hp-', h, hh); h = hh;
         if(m != mm) setAnalog('mp-', m, mm); m = mm;
         setAnalog('sp-', s, ss); s = ss;
     });
-}, 1000);
+}, 500);
---
+++ classes/head.js
@@ -0,0 +1,21 @@
+var d = document, h, m, s;
+
+Date().toString().replace(/(\d{2}):(\d{2}):(\d{2})/, function(match, hh, mm, ss) {
+    h = hh, m = mm, s = ss;
+    let style = document.createElement('style');
+    style.innerText = "#hp-" + hh + " { display: block; } #mp-" + mm + " { display: block; }  #sp-" + ss + " { display: block; }";
+    document.head.appendChild(style);
+});
+
+// Format Digital Number
+function setNumber(i, n) {
+    for(let p = 0; p < 7; p++) {
+        d.getElementById(i + p).style.display = maps[n][p];
+    }
+}
+
+// Set position of Pointer
+function setAnalog(i, oN, n) {
+    d.getElementById(i + oN).style.display = 'none';
+    d.getElementById(i + n).style.display = 'block';
+}
